479c9947b92e75d26814b06e805dcc0b924dc1fd
hvkcoder <hvkcoder@163.com>
2017-12-12 18:03:40 +0800

    Update Canvas Writing

--- Canvas/Writing/assets/js/app.js
+++ Canvas/Writing/assets/js/app.js
@@ -1,4 +1,42 @@
 'use strict'
 ;(function(){
-    console.log('fdsafdsaf');
+    var Writing ={};
+
+    /**
+     * 初始化 Writing 对象
+     * @param {String} canvas 
+     * @param {Number} width 
+     * @param {Number} height 
+     * @returns {Object}
+     */
+    Writing.init = function(canvas,width,height){
+        this.canvas = document.querySelector(canvas);
+        this.context = null;
+        if(this.canvas){
+            this.context = this.canvas.getContext('2d');
+            this.canvas.width = width;
+            this.canvas.height = height;
+        }
+        // this.isShowBorder = true;
+        // this.strokeColor = '#000';
+        this.dragging = false;
+        this.downLoc = { x : 0, y : 0 }
+        return this;
+    };
+
+    Writing.initCanvasBorder = function(){
+    }
+    window.writing = Writing;
 })();
+
+
+window.onload = function(){
+
+    // 初始化 writing
+    var writing = window.writing.init('#canvas',400,400);
+
+    //动态绑定事件
+    document.querySelector('.color-bar').addEventListener('click',function(e){
+        
+    },false);
+}
