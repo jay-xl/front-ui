9a10516499dd5e7bab77ed490446020fe8bf0e6c
hvkcoder <hvkcoder@163.com>
2017-12-13 10:29:46 +0800

    Update  Canvas Writing

--- Canvas/Writing/assets/js/app.js
+++ Canvas/Writing/assets/js/app.js
@@ -17,8 +17,8 @@
             this.canvas.width = width;
             this.canvas.height = height;
         }
-        // this.isShowBorder = true;
-        // this.strokeColor = '#000';
+        this.isShowBorder = true;
+        this.strokeColor = '#000';
         this.dragging = false;
         this.downLoc = { x : 0, y : 0 }
         return this;
@@ -35,8 +35,21 @@ window.onload = function(){
     // 初始化 writing
     var writing = window.writing.init('#canvas',400,400);
 
-    //动态绑定事件
+    // 动态绑定事件
     document.querySelector('.color-bar').addEventListener('click',function(e){
-        
+        var target = e.target;
+        // 绑定画笔颜色事件
+        if(target.localName === 'li' && !target.getAttribute('data-click')){
+            var colorBarList = document.querySelectorAll('.color-bar li');
+            // 设置画笔颜色
+            writing.strokeColor = target.innerText;
+            // 删除所有选中样式
+            for(let i = 0; i < colorBarList.length; i++){
+                colorBarList[i].classList.remove('active');
+            }
+            // 给点击的 target 添加 选中的样式
+            target.classList.add('active');
+        }
+
     },false);
 }
