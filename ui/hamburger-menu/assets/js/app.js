9257af7ee22138b23b186d8b0909a51ef5247fba
hvkcoder <hvkcoder@163.com>
2018-01-07 22:55:05 +0800

    reset ui hamburger-menu style

--- ui/hamburger-menu/assets/js/app.js
+++ ui/hamburger-menu/assets/js/app.js
@@ -1,16 +1,16 @@
-'use strict';
+// 'use strict';
 
-(function(){
-    var liList = document.querySelectorAll('li');
-    for(let i = 0; i < liList.length; i++){
-        liList[i].addEventListener('click',function(){
-            if(this.classList.contains('open')){
-                this.classList.remove('open');
-                this.classList.add('close');
-            }else{
-                this.classList.remove('close');
-                this.classList.add('open');
-            }
-        },false);
-    }
-})();
+// (function(){
+//     var liList = document.querySelectorAll('li');
+//     for(let i = 0; i < liList.length; i++){
+//         liList[i].addEventListener('click',function(){
+//             if(this.classList.contains('open')){
+//                 this.classList.remove('open');
+//                 this.classList.add('close');
+//             }else{
+//                 this.classList.remove('close');
+//                 this.classList.add('open');
+//             }
+//         },false);
+//     }
+// })();
