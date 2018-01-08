47d3075ae3c1f5f559d75c5a710a132ebe14d253
hvkcoder <hvkcoder@163.com>
2018-01-08 08:39:36 +0800

    Update ui hamburger-menu

--- ui/hamburger-menu/assets/js/app.js
+++ ui/hamburger-menu/assets/js/app.js
@@ -1,16 +1,15 @@
-// 'use strict';
-
-// (function(){
-//     var liList = document.querySelectorAll('li');
-//     for(let i = 0; i < liList.length; i++){
-//         liList[i].addEventListener('click',function(){
-//             if(this.classList.contains('open')){
-//                 this.classList.remove('open');
-//                 this.classList.add('close');
-//             }else{
-//                 this.classList.remove('close');
-//                 this.classList.add('open');
-//             }
-//         },false);
-//     }
-// })();
+'use strict';
+(function(){
+    var sectionList = document.querySelectorAll('section');
+    for(let i = 0; i < sectionList.length; i++){
+        sectionList[i].addEventListener('click',function(){
+            if(this.classList.contains('open')){
+                this.classList.remove('open');
+                this.classList.add('close');
+            }else{
+                this.classList.remove('close');
+                this.classList.add('open');
+            }
+        },false);
+    };
+})();
