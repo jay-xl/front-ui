a4eccb7b57d14b936460bf5c5a7a10a64373332a
hvkcoder <hvkcoder@163.com>
2018-02-27 23:55:18 +0800

    Update other/3d-product-display

--- other/3d-product-display/assets/js/app.js
+++ other/3d-product-display/assets/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
     const oBox = document.querySelector('.box');
-
+    let mouseDown = false;
     // 鼠标按下
     const mousePoint = { x: 0, y: 0 };
 
@@ -9,7 +9,22 @@
      * 鼠标按下事件
     */
     oBox.addEventListener('mousedown', function (e) {
-        console.log('fds');
+        mouseDown = true;
+        // 获取鼠标点击后的坐标
+        let downX = e.clientX;
+        let downY = e.clientY;
+
+        // 鼠标移动事件
+        document.addEventListener('mousemove',function(e){
+            if(mouseDown){
+                console.log('实时');
+            }
+        },false);
+
+        // 鼠标抬起事件
+        document.addEventListener('mouseup',function(){
+            mouseDown = false;
+        },false);
     }, false);
 
 })();
