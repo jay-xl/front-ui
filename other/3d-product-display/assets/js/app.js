b4475216f6f87d58ac29dd22d42f6a9834f2b2a6
hvkcoder <hvkcoder@163.com>
2018-02-28 08:10:39 +0800

    Update other/3d-product-display

--- other/3d-product-display/assets/js/app.js
+++ other/3d-product-display/assets/js/app.js
@@ -2,8 +2,8 @@
 (function () {
     const oBox = document.querySelector('.box');
     let mouseDown = false;
-    // 鼠标按下
-    const mousePoint = { x: 0, y: 0 };
+    // 旋转角度
+    const rotate = { x: 0, y: 0 };
 
     /**
      * 鼠标按下事件
@@ -15,16 +15,29 @@
         let downY = e.clientY;
 
         // 鼠标移动事件
-        document.addEventListener('mousemove',function(e){
-            if(mouseDown){
-                console.log('实时');
+        document.addEventListener('mousemove', function (e) {
+            if (mouseDown) {
+                // 计算鼠标移动的坐标
+                let moveX = e.clientX - downX;
+                let moveY = e.clientY - downY;
+
+                // 更改鼠标点击的坐标为移动坐标
+                downX = e.clientX;
+                downY = e.clientY;
+
+                // 计算旋转角度
+                rotate.x += moveX * 0.2;
+                rotate.y += moveY * 0.2;
+
+                // 设置 box 旋转样式
+                oBox.style.transform = 'rotateY(' + rotate.x + 'deg) rotateX(' + rotate.y + 'deg)';
             }
-        },false);
+        }, false);
 
         // 鼠标抬起事件
-        document.addEventListener('mouseup',function(){
+        document.addEventListener('mouseup', function () {
             mouseDown = false;
-        },false);
+        }, false);
     }, false);
 
 })();
