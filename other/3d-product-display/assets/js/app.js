b0b44a9a5d632ad1e3b920cc6ac5d8c4833a11bb
hvkcoder <hvkcoder@163.com>
2018-02-28 23:10:00 +0800

    Update other/3d-product-display

--- other/3d-product-display/assets/js/app.js
+++ other/3d-product-display/assets/js/app.js
@@ -1,43 +1,32 @@
-'use strict';
-(function () {
-    const oBox = document.querySelector('.box');
-    let mouseDown = false;
-    // 旋转角度
-    const rotate = { x: 0, y: 0 };
+window.onload = function() {
 
-    /**
-     * 鼠标按下事件
-    */
-    oBox.addEventListener('mousedown', function (e) {
-        mouseDown = true;
-        // 获取鼠标点击后的坐标
-        let downX = e.clientX;
-        let downY = e.clientY;
 
-        // 鼠标移动事件
-        document.addEventListener('mousemove', function (e) {
-            if (mouseDown) {
-                // 计算鼠标移动的坐标
-                let moveX = e.clientX - downX;
-                let moveY = e.clientY - downY;
+    var distX, degX = 0,
+        distY, degY = 0;
+    var oBox = document.querySelector('.box');
 
-                // 更改鼠标点击的坐标为移动坐标
-                downX = e.clientX;
-                downY = e.clientY;
+    oBox.onmousedown = function(e) {
+        var sX = e.clientX,
+            sY = e.clientY;
 
-                // 计算旋转角度
-                rotate.x += moveX * 0.2;
-                rotate.y -= moveY * 0.2;
+        document.body.style.cursor = 'url("data:image/gif;base64,R0lGODlhEAAQAJECAAAAAP///////wAAACH5BAEAAAIALAAAAAAQABAAQAI3lC8AeBDvgosQxQtne7yvLWGStVBelXBKqDJpNzLKq3xWBlU2nUs4C/O8cCvU0EfZGUwt19FYAAA7"), ew-resize';
 
-                // 设置 box 旋转样式
-                oBox.style.transform = 'rotateY(' + rotate.x + 'deg) rotateX(' + rotate.y + 'deg)';
-            }
-        }, false);
+        document.onmousemove = function(e) {
+            distX = e.clientX - sX;
+            distY = e.clientY - sY;
 
-        // 鼠标抬起事件
-        document.addEventListener('mouseup', function () {
-            mouseDown = false;
-        }, false);
-    }, false);
+            sX = e.clientX;
+            sY = e.clientY;
 
-})();
+            degX += distX * .2;
+            degY -= distY * .2;
+
+            oBox.style.transform = 'rotateY(' + degX + 'deg) rotateX(' + degY + 'deg)';
+        }
+    }
+    document.onmouseup = function() {
+        document.onmousemove = null;
+        document.body.style.cursor = 'default';
+
+    }
+}
